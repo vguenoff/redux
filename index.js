94032c9507e836da1f44e57eb23cf9d5f902dd2a
Vassil Guenoff <vguenoff@gmail.com>
2018-04-12 16:21:30 +0300

    Laveraging constants and action creators

--- index.js
+++ index.js
@@ -1,51 +1,4 @@
-/*
-Characteristics of a Pure Function:
-1) They always return the same results if the same arguments are passed in.
-2) They depend only on the arguments passed into them.
-3) Never produce any side effects.
-*/
-
-// todos reducer function
-function todos(state = [], action) {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return state.concat(action.todo);
-        case 'REMOVE_TODO':
-            return state.filter(todo => todo.id !== action.id);
-        case 'TOGGLE_TODO':
-            return state.map(
-                todo =>
-                    todo.id !== action.id
-                        ? todo
-                        : {
-                              ...todo,
-                              complete: !todo.complete,
-                          }
-            );
-        default:
-            return state;
-    }
-}
-
-// goals reducer function
-function goals(state = [], action) {
-    switch (action.type) {
-        case 'ADD_GOAL':
-            return state.concat(action.goal);
-        case 'REMOVE_GOAL':
-            return state.filter(goal => goal.id !== action.id);
-        default:
-            return state;
-    }
-}
-
-function app(state = {}, action) {
-    return {
-        todos: todos(state.todos, action),
-        goals: goals(state.goals, action),
-    };
-}
-
+// library code
 function createStore(reducer) {
     // The store should have four parts:
     // 1. The state
@@ -79,61 +32,113 @@ function createStore(reducer) {
         dispatch,
     };
 }
+// end of library code
+
+/*
+note: Characteristics of a Pure Function:
+1) They always return the same results if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
+
+// App code
+// actions
+const addTodo = 'ADD_TODO';
+const removeTodo = 'REMOVE_TODO';
+const toggleTodo = 'TOGGLE_TODO';
+const addGoal = 'ADD_GOAL';
+const removeGoal = 'REMOVE_GOAL';
+
+// action creators
+const addTodoAction = todo => ({ type: addTodo, todo });
+const removeTodoAction = id => ({ type: removeTodo, id });
+const toggleTodoAction = id => ({ type: toggleTodo, id });
+const addGoalAction = goal => ({ type: addGoal, goal });
+const removeGoalAction = id => ({ type: removeGoal, id });
+
+// todos reducer function
+function todos(state = [], action) {
+    switch (action.type) {
+        case addTodo:
+            return state.concat(action.todo);
+        case removeTodo:
+            return state.filter(todo => todo.id !== action.id);
+        case toggleTodo:
+            return state.map(
+                todo =>
+                    todo.id !== action.id
+                        ? todo
+                        : {
+                              ...todo,
+                              complete: !todo.complete,
+                          }
+            );
+        default:
+            return state;
+    }
+}
+
+// goals reducer function
+function goals(state = [], action) {
+    switch (action.type) {
+        case addGoal:
+            return state.concat(action.goal);
+        case removeGoal:
+            return state.filter(goal => goal.id !== action.id);
+        default:
+            return state;
+    }
+}
+
+function app(state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action),
+    };
+}
 
 const store = createStore(app);
 const unsubscribe = store.subscribe(() => {
-    console.log('The new state will be', store.getState());
+    console.log('----------------------');
+    console.log('The new state will be:', store.getState());
 });
 
-store.dispatch({
-    type: 'ADD_TODO',
-    todo: {
+store.dispatch(
+    addTodoAction({
         id: 1,
         name: 'Learn React',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: 'ADD_TODO',
-    todo: {
+store.dispatch(
+    addTodoAction({
         id: 2,
         name: 'Learn Redux',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: 'REMOVE_TODO',
-    id: 2,
-});
+store.dispatch(removeTodoAction(2));
 
-store.dispatch({
-    type: 'TOGGLE_TODO',
-    id: 1,
-});
+store.dispatch(toggleTodoAction(1));
 
-store.dispatch({
-    type: 'ADD_GOAL',
-    goal: {
+store.dispatch(
+    addGoalAction({
         id: 1,
         name: 'Learn To Swim',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: 'ADD_GOAL',
-    goal: {
+store.dispatch(
+    addGoalAction({
         id: 2,
         name: 'Lose 5 kilos',
         complete: false,
-    },
-});
+    })
+);
 
-store.dispatch({
-    type: 'REMOVE_GOAL',
-    id: 2,
-});
+store.dispatch(removeGoalAction(2));
 
 unsubscribe();
